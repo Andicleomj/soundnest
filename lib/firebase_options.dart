cd6af0fe84902eff1318dda1f05d7d176e39690c
yubi <yubimadhaniasahid02@gmail.com>
2025-05-13 01:54:14 +0700

    update

---
+++ lib/firebase_options.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: avoid_classes_with_only_static_members
+
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+/// Konfigurasi Firebase untuk Android.
+class DefaultFirebaseOptions {
+  static const FirebaseOptions currentPlatform = android;
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyB9jwIptytxYZAkh8LjdNsaRak4vPP1wC',
+    appId: '1:307643391567:android:02b4ee9aecf759d220943a',
+    messagingSenderId: '307643391567',
+    projectId: 'databaseta-108d5',
+    storageBucket: 'databaseta-108d5.firebasestorage.app',
+    databaseURL: 'https://databaseta-108d5-default-rtdb.firebaseio.com',
+  );
+}
