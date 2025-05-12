588f4dff72aa10ea177470e318f783cb82c580e2
yubi <yubimadhaniasahid02@gmail.com>
2025-05-13 01:48:20 +0700

    terupdate

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:soundnest/firebase_options.dart';
 import 'package:soundnest/utils/app_routes.dart';
+import 'package:soundnest/service/notifikasi_service.dart'; // Import NotifikasiService
+import 'package:soundnest/service/schedule_service.dart'; // Import ScheduleService
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // Inisialisasi Firebase
+  // Inisialisasi Notifikasi
+  final notificationService = NotificationService();
+  await notificationService.initialize();
+
+  // Inisialisasi ScheduleService
+  final scheduleService = ScheduleService();
+  scheduleService.startScheduleChecker(); // Memulai pengecekan jadwal otomatis
+
+  runApp(MyApp(scheduleService: scheduleService));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final ScheduleService scheduleService;
+  const MyApp({super.key, required this.scheduleService});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SoundNest',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       initialRoute: AppRoutes.splash,
       routes: AppRoutes.getRoutes(),
     );
