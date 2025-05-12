3c103677a0e38f9133b3ae27386f903c8277a49c
yubi <yubimadhaniasahid02@gmail.com>
2025-05-12 18:58:48 +0700

    force update schedule_service.dart

--- lib/service/schedule_service.dart
+++ lib/service/schedule_service.dart
@@ -1,33 +1,26 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart'; // Pastikan ini untuk inisialisasi Firebase jika belum dilakukan sebelumnya
-import '../models/schedule_model.dart'; // Pastikan path import sesuai dengan struktur folder proyek kamu
+import 'package:firebase_database/firebase_database.dart';
+
+// update test : force commit
 
 class ScheduleService {
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final DatabaseReference _ref = FirebaseDatabase.instance.ref(
+    'devices/device_001/schedules',
+  );
 
-  // Inisialisasi Firebase jika belum dilakukan
-  Future<void> initialize() async {
-    await Firebase.initializeApp(); // Pastikan Firebase diinisialisasi sebelum menggunakan Firestore
-  }
-
-  // Mengambil jadwal dari Firestore
-  Future<List<Schedule>> getSchedules() async {
-    try {
-      final snapshot = await _firestore.collection('penjadwalan').get();
-
-      // Mengecek apakah data kosong
-      if (snapshot.docs.isEmpty) {
-        return [];
-      }
-
-      // Mengembalikan daftar Schedule yang dipetakan dari data Firestore
-      return snapshot.docs.map((doc) {
-        return Schedule.fromMap(doc.data() as Map<String, dynamic>);
-      }).toList();
-    } catch (e) {
-      // Menangani error dan mencetak pesan error
-      print("Error getting schedules: $e");
+  // Ambil semua jadwal
+  Future<List<Map<String, dynamic>>> getSchedules() async {
+    final snapshot = await _ref.get();
+    if (snapshot.exists) {
+      final data = Map<String, dynamic>.from(snapshot.value as Map);
+      return data.values.map((e) => Map<String, dynamic>.from(e)).toList();
+    } else {
       return [];
     }
   }
+
+  // Tambahkan jadwal baru
+  Future<void> addSchedule(Map<String, dynamic> scheduleData) async {
+    await _ref.push().set(scheduleData);
+  }
 }
+// tes
