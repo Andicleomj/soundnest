6117beb98a6ab601fe789fa8081aca626d93b38b
yubi <yubimadhaniasahid02@gmail.com>
2025-05-13 14:38:22 +0700

    terupdate

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:soundnest/main.dart';
-import 'package:soundnest/service/schedule_service.dart';
 
 void main() {
-  testWidgets('Test MyApp dengan ScheduleService', (WidgetTester tester) async {
-    // Inisiasi ScheduleService yang Dibutuhkan
-    final scheduleService = ScheduleService();
-
-    // Bangun aplikasi dengan ScheduleService
-    await tester.pumpWidget(MyApp(scheduleService: scheduleService));
+  testWidgets('Test MyApp tanpa ScheduleService dan NotificationService', (
+    WidgetTester tester,
+  ) async {
+    // Bangun aplikasi tanpa ScheduleService dan NotificationService
+    await tester.pumpWidget(const MyApp());
 
     // Verifikasi apakah widget muncul
     expect(find.text('Firebase Test'), findsOneWidget);
+
+    // Verifikasi tombol navigasi ke SignUp ada
+    expect(find.text('Go to Sign Up'), findsOneWidget);
+
+    // Simulasi klik tombol untuk navigasi
+    await tester.tap(find.text('Go to Sign Up'));
+    await tester.pumpAndSettle();
+
+    // Verifikasi apakah halaman SignUp muncul
+    expect(find.text('Sign Up'), findsOneWidget);
   });
 }
