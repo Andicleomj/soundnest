e5d10a23204829b80bff8be9e6fd352114316d1a
yubi <yubimadhaniasahid02@gmail.com>
2025-05-14 07:07:23 +0700

    dah

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:soundnest/firebase_options.dart';
 import 'package:soundnest/service/schedule_service.dart';
+import 'package:soundnest/service/music_player_service.dart'; // Import MusicPlayerService
+import 'package:soundnest/screens/home/musik/musik_screen.dart'; // Import MusicScreen
 import 'package:soundnest/utils/app_routes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -35,7 +37,12 @@ class MyApp extends StatelessWidget {
           FirebaseAuth.instance.currentUser == null
               ? AppRoutes.login
               : AppRoutes.home,
-      routes: AppRoutes.getRoutes(),
+      routes: {
+        ...AppRoutes.getRoutes(),
+        '/music':
+            (context) =>
+                const MusicScreen(), // Tambahkan MusicScreen sebagai route
+      },
     );
   }
 }
