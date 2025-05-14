5326cd68bf3163ed71852e1c02d23c8de915011b
AndiCleo MJ <andicleomj@gmail.com>
2025-05-14 19:27:25 +0700

    new

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,1 +1,34 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue.shade100,
+          elevation: 0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10), 
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(fontSize: 16, color: Colors.black),
+        ),
+      ),
+    );
+  }
+}
