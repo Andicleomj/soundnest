1770d23c3e54d509dcb4e197674fac75c0798575
yubi <yubimadhaniasahid02@gmail.com>
2025-05-15 16:53:25 +0700

    Save

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -63,7 +63,7 @@ class HomeScreen extends StatelessWidget {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => const MusicScreen(),
+                            builder: (context) => const MusikKategoriScreen(),
                           ),
                         );
                       },
