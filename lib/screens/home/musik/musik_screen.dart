863e5c9ef8a04f90698756f13907411b10553b06
yubi <yubimadhaniasahid02@gmail.com>
2025-05-16 02:00:58 +0700

    Y

--- lib/screens/home/musik/musik_screen.dart
+++ lib/screens/home/musik/musik_screen.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:soundnest/screens/home/musik/hewan_screen.dart';
 
 class MusicScreen extends StatelessWidget {
   const MusicScreen({super.key});
 
+  String getPathFromCategory(String category) {
+    switch (category) {
+      case 'Masa Adaptasi Sekolah':
+        return 'Masa Adaptasi Sekolah';
+      case 'Aku Suka Olahraga':
+        return 'Aku Suka Olahraga';
+      case 'My Family':
+        return 'My Family';
+      case 'Bumi Planet':
+        return 'Bumi Planet';
+      case 'Hari Kemerdekaan':
+        return 'Hari Kemerdekaan';
+      case 'Ramadhan':
+        return 'Ramadhan';
+      case 'Hewan':
+        return 'Hewan';
+      case 'Manasik Haji':
+        return 'Manasik Haji';
+      case 'Budaya Sunda':
+        return 'Budaya Sunda';
+      case 'Batik':
+        return 'Batik';
+      case 'Mother Day':
+        return 'Mother Day';
+      case 'Guruku Tersayang':
+        return 'Guruku Tersayang';
+      case 'Profesi':
+        return 'Profesi';
+      case 'Kendaraan':
+        return 'Kendaraan';
+      default:
+        return 'Surah Pendek';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Kategori Musik'),
-        backgroundColor: Colors.blue,
+        title: const Text(
+          'Kategori Musik',
+          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blueAccent, Colors.white],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+        ),
       ),
+
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: GridView.count(
@@ -30,7 +82,7 @@ class MusicScreen extends StatelessWidget {
             _buildCategoryCard(context, 'Mother Day'),
             _buildCategoryCard(context, 'Guruku Tersayang'),
             _buildCategoryCard(context, 'Profesi'),
-            _buildCategoryCard(context, 'Kendariaan'),
+            _buildCategoryCard(context, 'Kendaraan'),
           ],
         ),
       ),
@@ -40,10 +92,13 @@ class MusicScreen extends StatelessWidget {
   Widget _buildCategoryCard(BuildContext context, String category) {
     return GestureDetector(
       onTap: () {
+        final path = getPathFromCategory(category);
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => MusicCategoryScreen(category: category),
+            builder:
+                (context) =>
+                    HewanScreen(categoryPath: path, categoryName: category),
           ),
         );
       },
