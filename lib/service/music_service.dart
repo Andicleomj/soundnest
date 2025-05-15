842e3da0e79638f91cfd18d1e2cbafcd7c20746c
yubi <yubimadhaniasahid02@gmail.com>
2025-05-15 14:44:51 +0700

    Simpan

--- lib/service/music_service.dart
+++ lib/service/music_service.dart
@@ -1,9 +1,25 @@
+// lib/service/music_service.dart
 import 'package:firebase_database/firebase_database.dart';
 
 class MusicService {
   final DatabaseReference _musicRef = FirebaseDatabase.instance.ref(
     'devices/devices_01/music/categories',
   );
+
+  Future<void> addMusic({
+    required String category,
+    required String title,
+    required String fileId,
+  }) async {
+    try {
+      final categoryRef = _musicRef.child('$category/files');
+      final newFileRef = categoryRef.push();
+      await newFileRef.set({'title': title, 'file_id': fileId});
+      print("✅ Musik berhasil ditambahkan.");
+    } catch (e) {
+      print("❌ Error menambahkan musik: $e");
+    }
+  }
 
   Future<List<Map<String, dynamic>>> getMusicByCategory(
     String categoryId,
