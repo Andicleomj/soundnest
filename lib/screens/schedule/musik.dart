7141f73be99ba506e2b6f2a164a89e681f43ce32
Yubi Madhania <yubimadhaniasahid02@gmail.com>
2025-06-16 03:36:10 +0700

    Update musik.dart

--- lib/screens/schedule/musik.dart
+++ lib/screens/schedule/musik.dart
@@ -97,6 +97,50 @@ class _MusikScheduleFormState extends State<MusikScheduleForm> {
 
   @override
   Widget build(BuildContext context) {
+
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+ List<Map<String, dynamic>> stars = [
+  {'top': 30.0, 'left': 20.0, 'size': 25.0, 'color': Colors.pinkAccent},
+  {'top': 60.0, 'left': screenWidth * 0.3, 'size': 20.0, 'color': Colors.lightBlueAccent},
+  {'top': 90.0, 'left': screenWidth * 0.65, 'size': 35.0, 'color': Colors.greenAccent},
+
+  {'top': screenHeight * 0.2, 'left': screenWidth * 0.1, 'size': 22.0, 'color': Colors.yellowAccent},
+  {'top': screenHeight * 0.22, 'left': screenWidth * 0.75, 'size': 28.0, 'color': Colors.orangeAccent},
+  {'top': screenHeight * 0.3, 'left': screenWidth * 0.4, 'size': 60.0, 'color': Colors.blueAccent},
+
+  {'top': screenHeight * 0.4, 'left': screenWidth * 0.15, 'size': 24.0, 'color': Colors.white},
+  {'top': screenHeight * 0.45, 'left': screenWidth * 0.7, 'size': 30.0, 'color': Colors.purpleAccent},
+  {'top': screenHeight * 0.5, 'left': screenWidth * 0.5, 'size': 40.0, 'color': Colors.pinkAccent},
+
+  {'top': screenHeight * 0.6, 'left': screenWidth * 0.25, 'size': 30.0, 'color': Colors.white},
+  {'top': screenHeight * 0.68, 'left': screenWidth * 0.6, 'size': 45.0, 'color': Colors.cyanAccent},
+  {'top': screenHeight * 0.72, 'left': screenWidth * 0.15, 'size': 35.0, 'color': Colors.purpleAccent},
+
+  {'top': screenHeight * 0.8, 'left': screenWidth * 0.8, 'size': 50.0, 'color': Colors.greenAccent},
+  {'top': screenHeight * 0.85, 'left': screenWidth * 0.4, 'size': 40.0, 'color': Colors.amberAccent},
+  {'top': screenHeight * 0.7, 'left': screenWidth * 0.9, 'size': 22.0, 'color': Colors.pinkAccent},
+];
+
+
+ Widget buildStar({required double size, required Color color}) {
+  return ShaderMask(
+    shaderCallback: (bounds) {
+      return RadialGradient(
+        colors: [color.withOpacity(0.9), color.withOpacity(0)],
+        stops: const [0.0, 1.0],
+      ).createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height));
+    },
+    blendMode: BlendMode.srcATop,
+    child: Icon(
+      Icons.star,
+      size: size,
+      color: Colors.white, // warna dasar sebelum shader diterapkan
+    ),
+  );
+}
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -117,7 +161,36 @@ class _MusikScheduleFormState extends State<MusikScheduleForm> {
         centerTitle: true,
       ),
 
-      body: Padding(
+       body: Stack(
+      children: [
+        // Background gradient
+        Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color(0xFFDCF1FF),
+                Color(0xFFA4D6FF),
+                Color(0xFFF2F9FD),
+              ],
+            ),
+          ),
+        ),
+        // Bintang-bintang
+        ...stars.map((pos) => Positioned(
+              top: pos['top'],
+              left: pos['left'],
+              child: buildStar(size: pos['size'], color: pos['color']),
+            )),
+
+             Container(
+      color: Colors.white.withOpacity(0.2),
+    ),
+
+    Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
@@ -213,6 +286,8 @@ class _MusikScheduleFormState extends State<MusikScheduleForm> {
           ],
         ),
       ),
+      ],
+       ),
     );
   }
 }
